f940426776793d457c0853ee24434a2b430efc4f
skoller <nskoller@gmail.com>
2019-12-16 16:41:56 -0500

    Initial Commit

---
+++ psychstn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  psychstn
+//
+//  Created by Nathan Skoller on 12/16/19.
+//  Copyright © 2019 Nathan Skoller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
